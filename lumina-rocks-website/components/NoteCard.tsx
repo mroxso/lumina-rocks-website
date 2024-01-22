fadc87c2673e44fa83c5e0d9c4654d7cded5f11c
mroxso <24775431+mroxso@users.noreply.github.com>
2024-01-22 22:29:49 +0100

    display id, pubkey and content

--- lumina-rocks-website/components/NoteCard.tsx
+++ lumina-rocks-website/components/NoteCard.tsx
@@ -17,7 +17,7 @@ const NoteCard: React.FC<NoteCardProps> = ({ pubkey, text, eventId }) => {
   const title = userData?.username || userData?.display_name || userData?.name || userData?.npub;
   // const imageSrc = text.match(/https?:\/\/.*\.(?:png|jpg|gif)/g)?.[0];
   const imageSrc = text.match(/https?:\/\/.*\.(?:png|jpg|gif)/g)?.[0].split(' ');
-
+  const textWithoutImage = text.replace(/https?:\/\/.*\.(?:png|jpg|gif)/g, '');
 
   return (
     <Card style={{ margin: '1rem' }}>
@@ -26,9 +26,15 @@ const NoteCard: React.FC<NoteCardProps> = ({ pubkey, text, eventId }) => {
         <Card.Text>
           {
             /* {text} */
-            imageSrc ? <img src={imageSrc[0]} style={{ maxWidth: '100%' }} /> : text
+            // imageSrc ? <img src={imageSrc[0]} style={{ maxWidth: '100%' }} /> : text
+            imageSrc ? imageSrc.map((src, index) => <img key={index} src={src} style={{ maxWidth: '100%' }} />) : ""
           }
+          {textWithoutImage}
         </Card.Text>
+        <Card.Footer>
+          <small className="text-muted">ID: {eventId}</small><br />
+          <small className="text-muted">Pubkey: {pubkey}</small>
+        </Card.Footer>
         {/* <Button variant="primary">Go somewhere</Button> */}
       </Card.Body>
     </Card>
