d042323c7451ba2e5ec33b7871384edbd778400a
mroxso <24775431+mroxso@users.noreply.github.com>
2024-01-28 22:16:15 +0100

    Reaction coming soon info

--- lumina/components/ReactionButton.tsx
+++ lumina/components/ReactionButton.tsx
@@ -7,6 +7,16 @@ import {
     getSignature,
 } from "nostr-tools";
 import { Button } from "@/components/ui/button";
+import {
+    Drawer,
+    DrawerClose,
+    DrawerContent,
+    DrawerDescription,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerTitle,
+    DrawerTrigger,
+} from "@/components/ui/drawer"
 
 export default function ReactionButton(event: any) {
     const { events } = useNostrEvents({
@@ -19,40 +29,58 @@ export default function ReactionButton(event: any) {
         },
     });
 
-    const { publish } = useNostr();
+    // const { publish } = useNostr();
 
-    const onPost = async () => {
-        const privKey = prompt("Paste your private key:");
+    // const onPost = async () => {
+    //     const privKey = prompt("Paste your private key:");
 
-        if (!privKey) {
-            alert("no private key provided");
-            return;
-        }
+    //     if (!privKey) {
+    //         alert("no private key provided");
+    //         return;
+    //     }
 
-        const message = prompt("Enter the message you want to send:");
+    //     const message = prompt("Enter the message you want to send:");
 
-        if (!message) {
-            alert("no message provided");
-            return;
-        }
+    //     if (!message) {
+    //         alert("no message provided");
+    //         return;
+    //     }
 
-        const event: NostrEvent = {
-            content: message,
-            kind: 1,
-            tags: [],
-            created_at: dateToUnix(),
-            pubkey: getPublicKey(privKey),
-            id: "",
-            sig: ""
-        };
+    //     const event: NostrEvent = {
+    //         content: message,
+    //         kind: 1,
+    //         tags: [],
+    //         created_at: dateToUnix(),
+    //         pubkey: getPublicKey(privKey),
+    //         id: "",
+    //         sig: ""
+    //     };
 
-        event.id = getEventHash(event);
-        event.sig = getSignature(event, privKey);
+    //     event.id = getEventHash(event);
+    //     event.sig = getSignature(event, privKey);
 
-        publish(event);
-    };
+    //     publish(event);
+    // };
 
     return (
-        <Button variant="default" onClick={onPost}>{events.length} Reactions</Button>
+        <Drawer>
+            <DrawerTrigger>
+                {/* <Button variant="default" onClick={onPost}>{events.length} Reactions</Button> */}
+                <Button variant="default">{events.length} Reactions</Button>
+            </DrawerTrigger>
+            <DrawerContent>
+                <DrawerHeader>
+                    <DrawerTitle>Reacting is coming soon!</DrawerTitle>
+                    <DrawerDescription>Sorry, but this feature is not implemented yet. Please try again later.</DrawerDescription>
+                </DrawerHeader>
+                <DrawerFooter>
+                    <DrawerClose>
+                        <Button>Close</Button>
+                    </DrawerClose>
+                </DrawerFooter>
+            </DrawerContent>
+        </Drawer>
+
+        // <Button variant="default" onClick={onPost}>{events.length} Reactions</Button>
     );
 }
