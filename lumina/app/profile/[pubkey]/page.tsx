22646f30139b99a491dfd17d45b9b4b8e41b8635
mroxso <24775431+mroxso@users.noreply.github.com>
2024-02-04 17:36:34 +0100

    use full width of small screens for showing images

--- lumina/app/profile/[pubkey]/page.tsx
+++ lumina/app/profile/[pubkey]/page.tsx
@@ -35,7 +35,7 @@ export default function ProfilePage() {
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <div className="py-6 px-6">
+        <div className="py-6 md:px-6">
           <div className="pb-6">
             <ProfileInfoCard pubkey={pubkey.toString()} />
           </div>
