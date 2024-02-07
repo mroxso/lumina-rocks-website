2d07776cf131a6458928b36cf76c3906ae3b8504
mroxso <24775431+mroxso@users.noreply.github.com>
2024-02-07 07:36:04 +0100

    refactor nip05 component

--- lumina/components/nip05.tsx
+++ lumina/components/nip05.tsx
@@ -1,37 +1,40 @@
 import { CheckIcon, ReloadIcon } from "@radix-ui/react-icons";
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface NIP05Props {
     nip05: string;
     pubkey: string;
 }
 
-let isLoading = true
-let isValid = false
-
 const NIP05: React.FC<NIP05Props> = ({ nip05, pubkey }) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [isValid, setIsValid] = useState(false);
 
     let name = nip05.split('@')[0]
     let domain = nip05.split('@')[1]
 
-    // check if nip05 is valid
-    let response = fetch(`https://${domain}/.well-known/nostr.json?name=${name}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data.names[name])
-            if (data.names[name] === pubkey) {
-                isValid = true
-            } else {
-                isValid = false
-            }
-            isLoading = false
-        })
+    useEffect(() => {
+        fetch(`https://${domain}/.well-known/nostr.json?name=${name}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data.names[name] === pubkey) {
+                    setIsValid(true);
+                } else {
+                    setIsValid(false);
+                }
+                setIsLoading(false);
+            })
+    }, [nip05, pubkey]);
 
     return (
         <>
             <div style={{ display: 'flex', alignItems: 'center' }}>
-                {nip05}
-                {isLoading ? <ReloadIcon className="mx-2 h-4 w-4 animate-spin" /> : isValid ? <CheckIcon className="mx-2 h-4 w-4" /> : <span className="mx-2 text-red-500">❌</span>}
+                {nip05.length > 0 && 
+                <>
+                    {nip05}
+                    {isLoading ? <ReloadIcon className="mx-2 h-4 w-4 animate-spin" /> : isValid ? <CheckIcon className="mx-2 h-4 w-4" /> : <span className="mx-2 text-red-500">❌</span>}
+                </>
+                }
             </div>
         </>
     );
