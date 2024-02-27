57ca2eb4adef3a8caa5c18e7a11df273f2a7136a
mroxso <24775431+mroxso@users.noreply.github.com>
2024-02-27 22:04:28 +0100

    some improvements for the profile info card

--- lumina/components/ProfileInfoCard.tsx
+++ lumina/components/ProfileInfoCard.tsx
@@ -5,24 +5,24 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { AvatarImage } from '@radix-ui/react-avatar';
 import { Avatar } from '@/components/ui/avatar';
 import { ReloadIcon } from '@radix-ui/react-icons';
-import NIP05  from '@/components/nip05';
+import NIP05 from '@/components/nip05';
 
 interface ProfileInfoCardProps {
   pubkey: string;
 }
 
 const ProfileInfoCard: React.FC<ProfileInfoCardProps> = ({ pubkey }) => {
-  const { data: userData } = useProfile({
+  const { data: userData, isLoading } = useProfile({
     pubkey,
   });
 
-  const title = userData?.username || userData?.display_name || userData?.name || userData?.npub;
+  const title = userData?.username || userData?.display_name || userData?.name || userData?.npub || pubkey;
   const description = userData?.about?.replace(/(?:\r\n|\r|\n)/g, '<br>');
   const nip05 = userData?.nip05
   return (
     <>
       <div className='py-6'>
-        {description ? (
+        {!isLoading ? (
           <Card>
             <CardHeader>
               <CardTitle>
@@ -37,11 +37,26 @@ const ProfileInfoCard: React.FC<ProfileInfoCardProps> = ({ pubkey }) => {
                 <NIP05 nip05={nip05?.toString() ?? ''} pubkey={pubkey} />
               </CardDescription>
             </CardHeader>
-            <CardContent><div className='pt-6 break-words' dangerouslySetInnerHTML={{ __html: description }} ></div></CardContent>
+            <CardContent><div className='pt-6 break-words' dangerouslySetInnerHTML={{ __html: description ?? '' }} ></div></CardContent>
           </Card>
         ) : (
           <div className="flex flex-col space-y-3">
-            <Skeleton className="h-[125px] rounded-xl" />
+            <Card>
+              <CardHeader>
+                <CardTitle>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Avatar className='mr-2'>
+                      <AvatarImage src={userData?.picture} alt={title} />
+                    </Avatar>
+                    {title}
+                  </div>
+                </CardTitle>
+                <CardDescription>
+                  {/* <NIP05 nip05={nip05?.toString() ?? ''} pubkey={pubkey} /> */}
+                </CardDescription>
+              </CardHeader>
+              <CardContent><div className='pt-6 break-words'></div></CardContent>
+            </Card>
           </div>
         )}
       </div>
