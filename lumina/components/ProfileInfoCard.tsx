e21fe04721d29d8a0fee9410c2de106c738b71ad
mroxso <24775431+mroxso@users.noreply.github.com>
2024-03-07 20:15:13 +0100

    improve profile card pt1

--- lumina/components/ProfileInfoCard.tsx
+++ lumina/components/ProfileInfoCard.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
-import { useNostrEvents, useProfile } from "nostr-react";
-import { Card, CardHeader, CardTitle, CardContent, CardFooter, CardDescription } from '@/components/ui/card';
-import { Skeleton } from '@/components/ui/skeleton';
+import React, { useMemo } from 'react';
+import { useProfile } from "nostr-react";
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
 import { AvatarImage } from '@radix-ui/react-avatar';
 import { Avatar } from '@/components/ui/avatar';
 import NIP05 from '@/components/nip05';
@@ -12,84 +11,45 @@ interface ProfileInfoCardProps {
   pubkey: string;
 }
 
-const ProfileInfoCard: React.FC<ProfileInfoCardProps> = ({ pubkey }) => {
-  const { data: userData, isLoading } = useProfile({
-    pubkey,
-  });
+const ProfileInfoCard: React.FC<ProfileInfoCardProps> = React.memo(({ pubkey }) => {
+  const { data: userData, isLoading } = useProfile({ pubkey });
 
-  // const { events: followers } = useNostrEvents({
-  //   filter: {
-  //     kinds: [3],
-  //     '#p': [pubkey],
-  //   },
-  // });
+  const npubShortened = useMemo(() => {
+    let encoded = nip19.npubEncode(pubkey);
+    let parts = encoded.split('npub');
+    return 'npub' + parts[1].slice(0, 4) + ':' + parts[1].slice(-3);
+  }, [pubkey]);
 
-  // const { events: following } = useNostrEvents({
-  //   filter: {
-  //     kinds: [3],
-  //     authors: [pubkey],
-  //   },
-  // });
-
-  let encoded = nip19.npubEncode(pubkey);
-  let parts = encoded.split('npub');
-  let npubShortened = 'npub' + parts[1].slice(0, 4) + ':' + parts[1].slice(-3);
-  const title = userData?.username || userData?.display_name || userData?.name || userData?.npub || npubShortened;
+  const title = userData?.username ?? userData?.display_name ?? userData?.name ?? userData?.npub ?? npubShortened;
   const description = userData?.about?.replace(/(?:\r\n|\r|\n)/g, '<br>');
-  const nip05 = userData?.nip05
+  const nip05 = userData?.nip05;
+
   return (
-    <>
-      <div className='py-6'>
-        {!isLoading ? (
-          <Card>
-            <CardHeader>
-              <CardTitle>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Avatar className='mr-2'>
-                    <AvatarImage src={userData?.picture} alt={title} />
-                  </Avatar>
-                  {title}
-                </div>
-              </CardTitle>
-              <CardDescription>
-                <NIP05 nip05={nip05?.toString() ?? ''} pubkey={pubkey} />
-                <div className=''>
-                  <div className='py-6'>
-                    {/* Followers: {followers.length} | Following: {following.length > 0 ? following[0]?.tags.length : "n/a"} | <Link href={`/dashboard/${pubkey}`}>view statistics</Link> */}
-                    <Link href={`/dashboard/${pubkey}`}>View Statistics</Link>
-                  </div>
-                  <hr />
-                </div>
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <div className='break-words' dangerouslySetInnerHTML={{ __html: description ?? '' }} >
-              </div>
-            </CardContent>
-          </Card>
-        ) : (
-          <div className="flex flex-col space-y-3">
-            <Card>
-              <CardHeader>
-                <CardTitle>
-                  <div style={{ display: 'flex', alignItems: 'center' }}>
-                    <Avatar className='mr-2'>
-                      <AvatarImage src={userData?.picture} alt={title} />
-                    </Avatar>
-                    {title}
-                  </div>
-                </CardTitle>
-                <CardDescription>
-                  {/* <NIP05 nip05={nip05?.toString() ?? ''} pubkey={pubkey} /> */}
-                </CardDescription>
-              </CardHeader>
-              <CardContent><div className='pt-6 break-words'></div></CardContent>
-            </Card>
+    <div className='py-6'>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <Avatar className='mr-2'>
+                <AvatarImage src={userData?.picture} alt={title} />
+              </Avatar>
+              {title}
+            </div>
+          </CardTitle>
+          <div>
+            <NIP05 nip05={nip05?.toString() ?? ''} pubkey={pubkey} />
+            <div className='py-6'>
+              <Link href={`/dashboard/${pubkey}`}>View Statistics</Link>
+            </div>
+            <hr />
           </div>
-        )}
-      </div>
-    </>
+        </CardHeader>
+        <CardContent>
+          <div className='break-words' dangerouslySetInnerHTML={{ __html: description ?? '' }} />
+        </CardContent>
+      </Card>
+    </div>
   );
-}
+});
 
 export default ProfileInfoCard;
