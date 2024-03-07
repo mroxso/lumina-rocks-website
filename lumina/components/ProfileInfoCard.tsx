6e576efa70fd4f53ccc6b7609c1d5e23140dfca6
mroxso <24775431+mroxso@users.noreply.github.com>
2024-03-07 19:48:37 +0100

    rm followers and following (for now) because it loads too long and shorten displayed npub

--- lumina/components/ProfileInfoCard.tsx
+++ lumina/components/ProfileInfoCard.tsx
@@ -5,6 +5,8 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { AvatarImage } from '@radix-ui/react-avatar';
 import { Avatar } from '@/components/ui/avatar';
 import NIP05 from '@/components/nip05';
+import { nip19 } from "nostr-tools";
+import Link from 'next/link';
 
 interface ProfileInfoCardProps {
   pubkey: string;
@@ -15,21 +17,24 @@ const ProfileInfoCard: React.FC<ProfileInfoCardProps> = ({ pubkey }) => {
     pubkey,
   });
 
-  const { events: followers } = useNostrEvents({
-    filter: {
-      kinds: [3],
-      '#p': [pubkey],
-    },
-  });
+  // const { events: followers } = useNostrEvents({
+  //   filter: {
+  //     kinds: [3],
+  //     '#p': [pubkey],
+  //   },
+  // });
 
-  const { events: following } = useNostrEvents({
-    filter: {
-      kinds: [3],
-      authors: [pubkey],
-    },
-  });
+  // const { events: following } = useNostrEvents({
+  //   filter: {
+  //     kinds: [3],
+  //     authors: [pubkey],
+  //   },
+  // });
 
-  const title = userData?.username || userData?.display_name || userData?.name || userData?.npub || pubkey;
+  let encoded = nip19.npubEncode(pubkey);
+  let parts = encoded.split('npub');
+  let npubShortened = 'npub' + parts[1].slice(0, 4) + ':' + parts[1].slice(-3);
+  const title = userData?.username || userData?.display_name || userData?.name || userData?.npub || npubShortened;
   const description = userData?.about?.replace(/(?:\r\n|\r|\n)/g, '<br>');
   const nip05 = userData?.nip05
   return (
@@ -50,8 +55,9 @@ const ProfileInfoCard: React.FC<ProfileInfoCardProps> = ({ pubkey }) => {
                 <NIP05 nip05={nip05?.toString() ?? ''} pubkey={pubkey} />
                 <div className=''>
                   <div className='py-6'>
-                    Followers: {followers.length} | Following: {following.length > 0 ? following[0]?.tags.length : "n/a"}
-                    </div>
+                    {/* Followers: {followers.length} | Following: {following.length > 0 ? following[0]?.tags.length : "n/a"} | <Link href={`/dashboard/${pubkey}`}>view statistics</Link> */}
+                    <Link href={`/dashboard/${pubkey}`}>View Statistics</Link>
+                  </div>
                   <hr />
                 </div>
               </CardDescription>
