3eb3001e94597ccee34dd4a9d822210571138cdb
mroxso <24775431+mroxso@users.noreply.github.com>
2024-04-03 16:34:22 +0200

    replace newline in trendingimage card

--- lumina/components/TrendingImage.tsx
+++ lumina/components/TrendingImage.tsx
@@ -27,9 +27,10 @@ const TrendingImage: React.FC<TrendingImageProps> = ({ eventId, pubkey }) => {
     },
   });
 
-  const text = events && events.length > 0 ? events[0].content : '';
+  let text = events && events.length > 0 ? events[0].content : '';
   const createdAt = events && events.length > 0 ? new Date(events[0].created_at * 1000) : new Date();
   const title = userData?.username || userData?.display_name || userData?.name || userData?.npub || nip19.npubEncode(pubkey);
+  text = text.replaceAll('\n', '<br />');
   const imageSrc = text.match(/https?:\/\/[^ ]*\.(png|jpg|gif)/g);
   const textWithoutImage = text.replace(/https?:\/\/.*\.(?:png|jpg|gif)/g, '');
   const hrefProfile = `/profile/${nip19.npubEncode(pubkey)}`;
