5cbe9005d35b99eda308a6f0467366238a32b5f7
mroxso <24775431+mroxso@users.noreply.github.com>
2024-04-06 08:57:56 +0200

    fix trending account and images to not crash website when not loading

--- lumina/components/TrendingAccounts.tsx
+++ lumina/components/TrendingAccounts.tsx
@@ -8,14 +8,16 @@ export function TrendingAccounts() {
         fetch('https://api.nostr.band/v0/trending/profiles')
             .then(res => res.json())
             .then(data => setProfiles(data.profiles))
-            .then();
+            .catch(error => {
+                console.error('Error calling trending profiles:', error);
+            });
     }, []);
 
     return (
         <div className="flex flex-col items-center py-6 px-6">
             <h1 className="text-3xl font-bold">Trending Accounts</h1>
             <div className="grid grid-cols-1 md:grid-cols-4 gap-4 mt-6">
-                {profiles.slice(0,4).map((profile, index) => (
+                {profiles && profiles.length > 0 && profiles.slice(0,4).map((profile, index) => (
                     <TrendingAccount key={index} pubkey={profile.pubkey} />
                 ))}
             </div>
--- lumina/components/TrendingImages.tsx
+++ lumina/components/TrendingImages.tsx
@@ -7,14 +7,17 @@ export function TrendingImages() {
     useEffect(() => {
         fetch('https://api.nostr.band/v0/trending/images')
             .then(res => res.json())
-            .then(data => setProfiles(data.images));
+            .then(data => setProfiles(data.images))
+            .catch(error => {
+                console.error('Error calling trending profiles:', error);
+            });
     }, []);
 
     return (
         <div className="flex flex-col items-center py-6 px-6">
             <h1 className="text-3xl font-bold">Trending Images</h1>
             <div className="grid grid-cols-2 md:grid-cols-4 gap-4 mt-6">
-                {profiles.map((profile, index) => (
+                {profiles && profiles.length > 0 && profiles.map((profile, index) => (
                     // <h1 key={index}>{profile.id}</h1>
                     <TrendingImage key={index} eventId={profile.id} pubkey={profile.pubkey} />
                 ))}
