e0c63326310a89e31419c90dc60d0692331fc1aa
mroxso <24775431+mroxso@users.noreply.github.com>
2024-05-01 02:08:08 +0200

    follow button action wip

--- lumina/components/FollowButton.tsx
+++ lumina/components/FollowButton.tsx
@@ -1,7 +1,10 @@
 
 import React, { useEffect, useState } from 'react';
 import { Button } from './ui/button';
-import { useNostrEvents } from 'nostr-react';
+import { useNostr, useNostrEvents } from 'nostr-react';
+import { finalizeEvent } from 'nostr-tools';
+import { sign } from 'crypto';
+import { SignalMedium } from 'lucide-react';
 
 interface FollowButtonProps {
     pubkey: string;
@@ -9,12 +12,16 @@ interface FollowButtonProps {
 }
 
 const FollowButton: React.FC<FollowButtonProps> = ({ pubkey, userPubkey }) => {
+    // const { publish } = useNostr();
     const [isFollowing, setIsFollowing] = useState(false);
 
+    let storedPubkey: string | null = null;
+    let storedNsec: string | null = null;
     let isLoggedIn = false;
     if (typeof window !== 'undefined') {
-        const pubkey = window.localStorage.getItem('pubkey');
-        isLoggedIn = pubkey !== null;
+        storedPubkey = window.localStorage.getItem('pubkey');
+        storedNsec = window.localStorage.getItem('nsec');
+        isLoggedIn = storedPubkey !== null;
     }
 
     const { events } = useNostrEvents({
@@ -24,22 +31,64 @@ const FollowButton: React.FC<FollowButtonProps> = ({ pubkey, userPubkey }) => {
             limit: 1,
         },
     });
+
     let followingPubkeys = events.flatMap((event) => event.tags.map(tag => tag[1]));
+    // filter out all null or undefined
+    followingPubkeys = followingPubkeys.filter((tag) => tag);
+
 
     useEffect(() => {
         if (followingPubkeys.includes(pubkey)) {
             setIsFollowing(true);
         }
-    }, [followingPubkeys, isFollowing]);
+    }, [followingPubkeys, isFollowing, setIsFollowing]);
 
-    const handleFollow = () => {
-        if (isLoggedIn) {
-            setIsFollowing(!isFollowing);
-        }
+    const handleFollow = async () => {
+    //     if (isLoggedIn) {
+
+    //         let eventTemplate = {
+    //             kind: 3,
+    //             created_at: Math.floor(Date.now() / 1000),
+    //             tags: [followingPubkeys],
+    //             content: '',
+    //         }
+
+    //         console.log(eventTemplate);
+
+    //         if (isFollowing) {
+    //             eventTemplate.tags = eventTemplate.tags.filter(tag => tag[1] !== pubkey);
+    //         } else {
+    //             eventTemplate.tags[0].push(pubkey);
+    //         }
+
+    //         console.log(eventTemplate);
+
+    //         let signedEvent = null;
+    //         if (storedNsec != null) {
+    //             // TODO: Sign Nostr Event with nsec
+    //             const nsecArray = storedNsec ? new TextEncoder().encode(storedNsec) : new Uint8Array();
+    //             signedEvent = finalizeEvent(eventTemplate, nsecArray);
+    //             console.log(signedEvent);
+    //         } else if (storedPubkey != null) {
+    //             // TODO: Request Extension to sign Nostr Event
+    //             console.log('Requesting Extension to sign Nostr Event..');
+    //             try {
+    //                 signedEvent = await window.nostr.signEvent(eventTemplate);
+    //             } catch (error) {
+    //                 console.error('Nostr Extension not found or aborted.');
+    //             }
+    //         }
+
+    //         if (signedEvent !== null) {
+    //             console.log(signedEvent);
+    //             publish(signedEvent);
+    //             setIsFollowing(!isFollowing);
+    //         }
+    //     }
     };
 
     return (
-        <Button className='w-full' onClick={handleFollow}>
+        <Button className='w-full' onClick={handleFollow} disabled>
             {isFollowing ? 'Unfollow' : 'Follow'}
         </Button>
     );
