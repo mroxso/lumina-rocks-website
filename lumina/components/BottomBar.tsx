b56bd3a94fad47e14893dfa11d954afd66b85e2f
mroxso <24775431+mroxso@users.noreply.github.com>
2024-05-13 22:53:44 +0200

    fix pathname highlighting

--- lumina/components/BottomBar.tsx
+++ lumina/components/BottomBar.tsx
@@ -20,21 +20,22 @@ export default function BottomBar() {
     return setPubkey(window.localStorage.getItem('pubkey') ?? null);
   }, []);
 
-  const isActive = (path: string) => usePathname() === path ? 'text-purple-500' : '';
+  const pathname = usePathname();
+  const isActive = (path: string, currentPath: string) => currentPath === path ? 'text-purple-500' : '';
 
   return (
     <nav className="fixed inset-x-0 bottom-0 h-14 flex flex-row shrink-0 items-center justify-between border-t bg-white shadow-up-4 dark:bg-gray-950 z-50">
-      <Link className={`flex flex-col items-center justify-center w-full text-xs gap-1 px-4 ${isActive('/')}`} href="/">
+      <Link className={`flex flex-col items-center justify-center w-full text-xs gap-1 px-4 ${isActive('/', pathname)}`} href="/">
         <HomeIcon className={`h-6 w-6`} />
         <span className="sr-only">Home</span>
       </Link>
       {pubkey && (
-        <Link className={`flex flex-col items-center justify-center w-full text-xs gap-1 px-4 ${isActive('/feed')}`} href="/feed">
+        <Link className={`flex flex-col items-center justify-center w-full text-xs gap-1 px-4 ${isActive('/feed', pathname)}`} href="/feed">
           <RowsIcon className={`h-6 w-6`} />
           <span className="sr-only">Follower Feed</span>
         </Link>
       )}
-      <Link className={`flex flex-col items-center justify-center w-full text-xs gap-1 px-4 ${isActive('/global')}`} href="/global">
+      <Link className={`flex flex-col items-center justify-center w-full text-xs gap-1 px-4 ${isActive('/global', pathname)}`} href="/global">
         <GlobeIcon className={`h-6 w-6`} />
         <span className="sr-only">Global</span>
       </Link>
