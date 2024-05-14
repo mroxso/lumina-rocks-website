77dbecd358d38fc1eea9561aa92aaa3f38c9cfe2
mroxso <24775431+mroxso@users.noreply.github.com>
2024-05-14 12:03:57 +0200

    feat: Update image and video filtering in ProfileFeed and ProfileQuickViewFeed
    The code changes modify the filtering logic in the `ProfileFeed` and `ProfileQuickViewFeed` components to include support for additional video file extensions (`mp4`, `webm`, `mov`). This ensures that both images and videos are properly filtered and displayed in the feed.

--- lumina/components/QuickViewNoteCard.tsx
+++ lumina/components/QuickViewNoteCard.tsx
@@ -9,6 +9,7 @@ import {
 } from "@/components/ui/card"
 import Image from 'next/image';
 import Link from 'next/link';
+import { VideoIcon } from '@radix-ui/react-icons';
 
 interface NoteCardProps {
   pubkey: string;
@@ -27,7 +28,8 @@ const QuickViewNoteCard: React.FC<NoteCardProps> = ({ pubkey, text, eventId, tag
   const title = userData?.username || userData?.display_name || userData?.name || userData?.npub || nip19.npubEncode(pubkey);
   text = text.replaceAll('\n', ' ');
   const imageSrc = text.match(/https?:\/\/[^ ]*\.(png|jpg|gif)/g);
-  const textWithoutImage = text.replace(/https?:\/\/.*\.(?:png|jpg|gif)/g, '');
+  const videoSrc = text.match(/https?:\/\/[^ ]*\.(mp4|webm|mov)/g);
+  const textWithoutImage = text.replace(/https?:\/\/.*\.(?:png|jpg|gif|mp4|webm|mov)/g, '');
   const createdAt = new Date(event.created_at * 1000);
   const hrefProfile = `/profile/${nip19.npubEncode(pubkey)}`;
   const profileImageSrc = userData?.picture || "https://robohash.org/" + pubkey;
@@ -43,6 +45,12 @@ const QuickViewNoteCard: React.FC<NoteCardProps> = ({ pubkey, text, eventId, tag
               // <div style={{ position: 'relative', width: '100%', maxHeight: '100vh' }}>
               //   <Image src={imageSrc[0]} alt={text} layout='fill' objectFit='contain' />
               // </div>
+            )}
+            {videoSrc && videoSrc.length > 0 && (
+              <div style={{ position: 'relative' }}>
+                <VideoIcon style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '50px', height: '50px' }} />
+                <video src={videoSrc[0] + "#t=0.5"} className='rounded lg:rounded-lg' style={{ maxWidth: '100%', maxHeight: '75vh', objectFit: 'contain', margin: 'auto' }} />
+              </div>
             )}
           </div>
         </div>
