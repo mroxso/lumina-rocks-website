e6422659b2e3aa0a4a09beb0a71bbd511b7bb981
mroxso <24775431+mroxso@users.noreply.github.com>
2024-05-18 08:34:27 +0200

    change video icon to play icon pt2

--- lumina/components/QuickViewNoteCard.tsx
+++ lumina/components/QuickViewNoteCard.tsx
@@ -42,7 +42,7 @@ const QuickViewNoteCard: React.FC<NoteCardProps> = ({ pubkey, text, eventId, tag
           <div className='d-flex justify-content-center align-items-center'>
             {imageSrc && imageSrc.length > 0 ? (
               <div style={{ position: 'relative' }}>
-                {videoSrc && videoSrc.length > 0 && <VideoIcon style={{ position: 'absolute', top: '10px', right: '10px', width: '50px', height: '50px' }} />}
+                {videoSrc && videoSrc.length > 0 && <PlayIcon style={{ position: 'absolute', top: '10px', right: '10px', width: '50px', height: '50px' }} />}
                 <img src={imageSrc[0]} className='rounded lg:rounded-lg' style={{ maxWidth: '100%', maxHeight: '75vh', objectFit: 'contain', margin: 'auto' }} alt={text} />
               </div>
             ) : videoSrc && videoSrc.length > 0 ? (
