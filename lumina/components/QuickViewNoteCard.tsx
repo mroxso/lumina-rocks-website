4a12f804f7101f3dfbe23bdc2ceee02529799781
mroxso <24775431+mroxso@users.noreply.github.com>
2024-05-18 08:38:06 +0200

    add StackIcon on Posts with more than one image

--- lumina/components/QuickViewNoteCard.tsx
+++ lumina/components/QuickViewNoteCard.tsx
@@ -9,7 +9,7 @@ import {
 } from "@/components/ui/card"
 import Image from 'next/image';
 import Link from 'next/link';
-import { PlayIcon, VideoIcon } from '@radix-ui/react-icons';
+import { PlayIcon, StackIcon, VideoIcon } from '@radix-ui/react-icons';
 
 interface NoteCardProps {
   pubkey: string;
@@ -37,24 +37,31 @@ const QuickViewNoteCard: React.FC<NoteCardProps> = ({ pubkey, text, eventId, tag
 
   const card = (
     <Card>
-      <SmallCardContent>
-        <div>
-          <div className='d-flex justify-content-center align-items-center'>
-            {imageSrc && imageSrc.length > 0 ? (
-              <div style={{ position: 'relative' }}>
-                {videoSrc && videoSrc.length > 0 && <PlayIcon style={{ position: 'absolute', top: '10px', right: '10px', width: '50px', height: '50px' }} />}
-                <img src={imageSrc[0]} className='rounded lg:rounded-lg' style={{ maxWidth: '100%', maxHeight: '75vh', objectFit: 'contain', margin: 'auto' }} alt={text} />
-              </div>
-            ) : videoSrc && videoSrc.length > 0 ? (
-              <div style={{ position: 'relative' }}>
-                <PlayIcon style={{ position: 'absolute', top: '10px', right: '10px', width: '50px', height: '50px' }} />
-                <video src={videoSrc[0] + "#t=0.5"} className='rounded lg:rounded-lg' style={{ maxWidth: '100%', maxHeight: '75vh', objectFit: 'contain', margin: 'auto' }} />
-              </div>
-            ) : null}
-          </div>
+    <SmallCardContent>
+      <div>
+        <div className='d-flex justify-content-center align-items-center'>
+          {imageSrc && imageSrc.length > 1 && !videoSrc ? (
+            <div style={{ position: 'relative' }}>
+              <StackIcon style={{ position: 'absolute', top: '10px', right: '10px', width: '50px', height: '50px' }} />
+              
+              <img src={imageSrc[0]} className='rounded lg:rounded-lg' style={{ maxWidth: '100%', maxHeight: '75vh', objectFit: 'contain', margin: 'auto' }} alt={text} />
+            </div>
+          ) : imageSrc && imageSrc.length > 0 ? (
+            <div style={{ position: 'relative' }}>
+              {videoSrc && videoSrc.length > 0 && <PlayIcon style={{ position: 'absolute', top: '10px', right: '10px', width: '50px', height: '50px' }} />}
+              
+              <img src={imageSrc[0]} className='rounded lg:rounded-lg' style={{ maxWidth: '100%', maxHeight: '75vh', objectFit: 'contain', margin: 'auto' }} alt={text} />
+            </div>
+          ) : videoSrc && videoSrc.length > 0 ? (
+            <div style={{ position: 'relative' }}>
+              <PlayIcon style={{ position: 'absolute', top: '10px', right: '10px', width: '50px', height: '50px' }} />
+              <video src={videoSrc[0] + "#t=0.5"} className='rounded lg:rounded-lg' style={{ maxWidth: '100%', maxHeight: '75vh', objectFit: 'contain', margin: 'auto' }} />
+            </div>
+          ) : null}
         </div>
-      </SmallCardContent>
-    </Card>
+      </div>
+    </SmallCardContent>
+  </Card>
   );
 
   return (
