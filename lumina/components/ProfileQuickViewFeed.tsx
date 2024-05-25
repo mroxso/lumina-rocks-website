1445fd531f2438f32262a8516fa0866e518b01f6
mroxso <24775431+mroxso@users.noreply.github.com>
2024-05-25 19:26:59 +0200

    implement load more button for ProfileQuickViewFeed (WIP)

--- lumina/components/ProfileQuickViewFeed.tsx
+++ lumina/components/ProfileQuickViewFeed.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useNostrEvents, dateToUnix } from "nostr-react";
 import { Skeleton } from "@/components/ui/skeleton";
 import QuickViewNoteCard from "./QuickViewNoteCard";
@@ -9,13 +9,12 @@ interface ProfileQuickViewFeedProps {
 
 const ProfileQuickViewFeed: React.FC<ProfileQuickViewFeedProps> = ({ pubkey }) => {
   const now = useRef(new Date()); // Make sure current time isn't re-rendered
+  const [limit, setLimit] = useState(20);
 
   const { events } = useNostrEvents({
     filter: {
-      // since: dateToUnix(now.current), // all new events from now
       authors: [pubkey],
-      // since: 0,
-      // limit: 500,
+      limit: limit,
       kinds: [1],
     },
   });
@@ -24,36 +23,41 @@ const ProfileQuickViewFeed: React.FC<ProfileQuickViewFeedProps> = ({ pubkey }) =
   // filter out all replies (tag[0] == e)
   filteredEvents = filteredEvents.filter((event) => !event.tags.some((tag) => { return tag[0] == 'e' }));
 
+  const loadMore = () => {
+    setLimit(limit => limit + 20);
+  }
+
   return (
     <>
       <div className="grid grid-cols-3 gap-2">
         {filteredEvents.length === 0 ? (
           <>
-          <div>
-            <Skeleton className="h-[125px] rounded-xl" />
-            <div className="space-y-2 py-2">
-              <Skeleton className="h-4 w-1/3" />
-              <Skeleton className="h-4 w-1/3" />
+            <div>
+              <Skeleton className="h-[125px] rounded-xl" />
+              <div className="space-y-2 py-2">
+                <Skeleton className="h-4 w-1/3" />
+                <Skeleton className="h-4 w-1/3" />
+              </div>
             </div>
-          </div>
-          <div>
-            <Skeleton className="h-[125px] rounded-xl" />
-            <div className="space-y-2 py-2">
-              <Skeleton className="h-4 w-1/3" />
-              <Skeleton className="h-4 w-1/3" />
+            <div>
+              <Skeleton className="h-[125px] rounded-xl" />
+              <div className="space-y-2 py-2">
+                <Skeleton className="h-4 w-1/3" />
+                <Skeleton className="h-4 w-1/3" />
+              </div>
             </div>
-          </div>
-          <div>
-            <Skeleton className="h-[125px] rounded-xl" />
-            <div className="space-y-2 py-2">
-              <Skeleton className="h-4 w-1/3" />
-              <Skeleton className="h-4 w-1/3" />
+            <div>
+              <Skeleton className="h-[125px] rounded-xl" />
+              <div className="space-y-2 py-2">
+                <Skeleton className="h-4 w-1/3" />
+                <Skeleton className="h-4 w-1/3" />
+              </div>
             </div>
-          </div>
           </>
         ) : (filteredEvents.map((event) => (
           <QuickViewNoteCard key={event.id} pubkey={event.pubkey} text={event.content} event={event} tags={event.tags} eventId={event.id} linkToNote={true} />
         )))}
+        <button onClick={loadMore}>Load More</button>
       </div>
     </>
   );
