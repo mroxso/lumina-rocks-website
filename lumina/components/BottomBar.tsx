9b507f8155059a8af0284f6e4e407524fba1a5a3
mroxso <24775431+mroxso@users.noreply.github.com>
2024-06-22 21:38:41 +0200

    upload (WIP)

--- lumina/components/BottomBar.tsx
+++ lumina/components/BottomBar.tsx
@@ -5,17 +5,91 @@
  * @see https://v0.dev/t/mwaJmHMv0vd
  * Documentation: https://v0.dev/docs#integrating-generated-code-into-your-nextjs-app
  */
-import { BellIcon, GlobeIcon, HomeIcon, RowsIcon } from "@radix-ui/react-icons"
+import { BellIcon, GlobeIcon, HomeIcon, RowsIcon, UploadIcon } from "@radix-ui/react-icons"
 import Link from "next/link"
-import { JSX, SVGProps, useEffect, useState } from "react"
+import { FormEvent, JSX, SVGProps, useEffect, useState } from "react"
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { useRouter, usePathname } from 'next/navigation'
 import { SearchIcon } from "lucide-react";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import { Button } from "./ui/button";
+import { Textarea } from "./ui/textarea";
 
 export default function BottomBar() {
 
   const router = useRouter();
   const [pubkey, setPubkey] = useState<null | string>(null);
+  const { createHash } = require('crypto');
+
+  async function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+  
+    const formData = new FormData(event.currentTarget);
+    const desc = formData.get('description') as string;
+    const file = formData.get('file') as File;
+  
+    const readFileAsArrayBuffer = (file: File): Promise<ArrayBuffer> => {
+      return new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onload = () => resolve(reader.result as ArrayBuffer);
+        reader.onerror = () => reject(reader.error);
+        reader.readAsArrayBuffer(file);
+      });
+    };
+  
+    try {
+      const arrayBuffer = await readFileAsArrayBuffer(file);
+      const hashBuffer = createHash('sha256').update(Buffer.from(arrayBuffer)).digest();
+      const sha256 = hashBuffer.toString('hex');
+  
+      const unixNow = () => Math.floor(Date.now() / 1000);
+      const newExpirationValue = () => (unixNow() + 60 * 5).toString();
+  
+      const pubkey = window.localStorage.getItem('pubkey');
+      const createdAt = Math.floor(Date.now() / 1000);
+  
+      let authEvent = {
+        kind: 24242,
+        content: desc,
+        created_at: createdAt,
+        tags: [
+          ['t', 'upload'],
+          ['x', sha256],
+          ['expiration', newExpirationValue()],
+        ],
+      };
+  
+      console.log(authEvent);
+  
+      let authEventSigned = await window.nostr.signEvent(authEvent);
+      console.log(authEventSigned);
+  
+      await fetch('https://media.lumina.rocks/upload', {
+        method: 'PUT',
+        body: file,
+        headers: { authorization: 'Nostr ' + btoa(JSON.stringify(authEventSigned)) },
+      }).then(async (res) => {
+        if (res.ok) {
+          alert(await res.text());
+
+          // TODO: Create and publish note
+        } else {
+          alert(await res.text());
+        }
+      });
+    } catch (error) {
+      console.error('Error reading file:', error);
+    }
+  }
 
   useEffect(() => {
     return setPubkey(window.localStorage.getItem('pubkey') ?? null);
@@ -36,10 +110,35 @@ export default function BottomBar() {
           <span className="sr-only">Follower Feed</span>
         </Link>
       )}
-      <Link className={`flex flex-col items-center justify-center w-full text-xs gap-1 px-4 ${isActive('/global', pathname)}`} href="/global">
-        <GlobeIcon className={`h-6 w-6`} />
-        <span className="sr-only">Global</span>
-      </Link>
+      {pubkey && (
+        <div className={`flex flex-col items-center justify-center w-full text-xs gap-1 px-4`}>
+          <Drawer>
+            <DrawerTrigger>
+              <div className="flex items-center justify-center h-10 w-10 rounded-full bg-purple-500">
+                <UploadIcon className={`h-6 w-6 text-white`} />
+              </div>
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerTitle>Upload</DrawerTitle>
+                <DrawerDescription>Post something</DrawerDescription>
+                <div>
+                  <form className="space-y-4" onSubmit={onSubmit}>
+                    <Textarea name="description" rows={6} placeholder="Your description" id="description" className="w-full"></Textarea>
+                    <input name="file" id="fie" type="file" accept="image/*" className="w-full" />
+                    <Button type="submit" className="w-full button">Upload</Button>
+                  </form>
+                </div>
+              </DrawerHeader>
+              <DrawerFooter>
+                <DrawerClose>
+                  <Button variant="outline">Cancel</Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+        </div>
+      )}
       <Link className={`flex flex-col items-center justify-center w-full text-xs gap-1 px-4 ${isActive('/search', pathname)}`} href="/search">
         <SearchIcon className={`h-6 w-6`} />
         <span className="sr-only">Search</span>
